f76f8a4776fd40ce4dfe2e833b6033842a59b26c
bard <begruset@gmail.com>
2025-05-11 20:40:46 +0200

    completed project

--- to_do.py
+++ to_do.py
@@ -6,7 +6,7 @@ todo = {"task1": False}
 
 
 def menu():
-    os.system("cls")
+    os.system("clear")
     print("1. Add a task")
     print("2. View tasks")
     print("3. Mark a task as complete")
@@ -16,7 +16,7 @@ def menu():
 
     def take_input():
         i = input("Please select an option (1-5): ")
-        os.system("cls")
+        os.system("clear")
         if i == "1":
             add_task()
         elif i == "2":
@@ -47,6 +47,7 @@ def add_task():
 
 def view_tasks():
     print("Here are your tasks:")
+    print()
     for task, done in todo.items():
         status = "Done" if done else "Not done"
         print(f"{task}: {status}")
@@ -60,7 +61,10 @@ def mark_task_complete():
     print("Please enter the task you want to mark as complete:")
     task = input()
     try:
-        todo.keys(task)
+        if todo[task]:
+            print("task is done")
+        else:
+            todo[task] = True
     except KeyError:
         print("Task not found.")
         print()
@@ -79,6 +83,16 @@ def mark_task_complete():
 def delete_task():
     print("Please enter the task you want to delete:")
     task = input()
+    try:
+        del todo[task]
+        print(f"Task: {task} has been deleted")
+        input("\nPress enter to continue...")
+        menu()
+    except KeyError:
+        print("Could not find task.")
+        input("\nPress enter to continue...")
+        menu()
+        return
 
     print()
     input("Press Enter to continue...")
